8b6bd387fdda9a11887bd512466bcd14e5826b2e
Ihor <kerass91@gmail.com>
2022-02-19 11:01:24 +0200

    new version from 19.02.2022

---
+++ src/js/script.js
@@ -0,0 +1,15 @@
+const slider = tns({
+    container: '.carousel_inner',
+    items: 1,
+    slideBy: 'page',
+    autoplay: false,
+    controls: false,
+    nav: false
+  });
+
+  document.querySelector('.prev').addEventListener('click', function () {
+    slider.goTo('prev');
+  });
+  document.querySelector('.next').addEventListener('click', function () {
+    slider.goTo('next');
+  });
