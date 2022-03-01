4622d68e12d802dcae1a120a5eb367f591c2d910
Ihor <kerass91@gmail.com>
2022-03-02 00:04:13 +0200

    01.02.2022

--- src/js/script.js
+++ src/js/script.js
@@ -21,15 +21,14 @@ const slider = tns({
     });
 
     function toggleSlide(item) {
-        $(item).each(function(i) {
-            $(this).on('click', function(e) {
-                e.preventDefault();
-                $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
-                $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
-            })
-        });
-    };
+      $(item).each(function(i) {
+          $(this).on('click', function(e) {
+              e.preventDefault();
+              $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+              $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+          })
+      });
+  };
 
-    toggleSlide('.catalog-item__link');
-    toggleSlide('.catalog-item__back');
-});
+  toggleSlide('.catalog-item__link');
+  toggleSlide('.catalog-item__back');
