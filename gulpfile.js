55b81996046e76d97dd639c68fe857f6d8158036
Ihor <kerass91@gmail.com>
2022-03-04 00:20:31 +0200

    00.20

--- gulpfile.js
+++ gulpfile.js
@@ -4,12 +4,14 @@ const sass        = require('gulp-sass')(require('sass'));
 const cleanCSS = require('gulp-clean-css');
 const autoprefixer = require('gulp-autoprefixer');
 const rename = require("gulp-rename");
+const imagemin = require('gulp-imagemin');
+const htmlmin = require('gulp-htmlmin');
 
 gulp.task('server', function() {
 
     browserSync({
         server: {
-            baseDir: "src"
+            baseDir: "dist"
         }
     });
 
@@ -22,12 +24,13 @@ gulp.task('styles', function() {
         .pipe(rename({suffix: '.min', prefix: ''}))
         .pipe(autoprefixer())
         .pipe(cleanCSS({compatibility: 'ie8'}))
-        .pipe(gulp.dest("src/css"))
+        .pipe(gulp.dest("dist/css"))
         .pipe(browserSync.stream());
 });
 
 gulp.task('watch', function() {
-    gulp.watch("src/sass/**/*.+(scss|sass)", gulp.parallel('styles'));
+    gulp.watch("src/sass/**/*.+(scss|sass|css)", gulp.parallel('styles'));
+    gulp.watch(".html").on('change', browserSync.reload);
 })
 
 gulp.task('default', gulp.parallel('watch', 'server', 'styles'));
--- src/js/script.js
+++ src/js/script.js
@@ -135,9 +135,11 @@ const slider = tns({
       $('.pageup').fadeOut();
     }
   })
-  $('a[href^="#"]').click(function(){
+  $('a[href^=#up]').click(function(){
     const _href = $(this).attr('href');
     $('html, body').animate({
     scrollTop: $(_href).offset().top+"px"});
     return false;
     });
+  
+  new WOW().init();
